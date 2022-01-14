0ffc1be683ea4946b684c6eabd8a3713c21f7ffe
mohammedpythons <mohammedshokai99@gmail.com>
2022-01-14 14:43:18 -0500

    finish user profile top part

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,21 +1,83 @@
-import GithubContext from "../../context/github/GithubContext"
+import { faCodepen, faStore, faUserFriends, Fausers } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import GithubContext from "../../context/github/GithubContext";
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const User = () => {
+  const { getUser, user, isLoading } = useContext(GithubContext);
+  const params = useParams();
+  const navigate = useNavigate();
 
-    const {getUser, user} = useContext(GithubContext);
-    const {login} = useParams();
+  useEffect(() => {
+    getUser(params.login);
+  }, []);
 
-useEffect(() => {
-    getUser(login);
-}, []);
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
 
-    return (
-        <div>
-
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div>
+      <div className="w-full mx-auto lg:w-10/12">
+        <div onClick={() => navigate(-1)} className="mb-4">
+          <button to="/" className="btn btn-ghost">
+            Back To Search
+          </button>
         </div>
-    )
-}
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+          <div className="custom-card-image mb-6 md:mb-0">
+            <div className="rounded-lg shadow-xl card image-full">
+              <figure>
+                <img src={avatar_url} alt="" />
+              </figure>
+              <div className="card-body jsutify-end">
+                <h2 className="card-title mb-0">{name}</h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                <div className="ml-2 mr-1 badge badge-success">{type}</div>
+                {hireable && (
+                  <div className="ms-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a
+                  href={html_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-outline"
+                >
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default User
+export default User;
